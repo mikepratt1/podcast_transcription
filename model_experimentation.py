9e3930e85c6b8ccc9a699395d8a1d6b708045d62
mikepratt1 <118982716+mikepratt1@users.noreply.github.com>
2023-05-16 11:07:13 +0100

    tested hugging face model

---
+++ model_experimentation.py
@@ -0,0 +1,35 @@
+"""
+model_experimentation
+
+- Getting to grips with the 'wav2vec2-large-xlsr-53-english' model from Hugging Face
+- I want to understand base level performance without any fine tuning 
+- The outcome of this script will help me determine if I need to fine-tune the model
+"""
+
+__date__ = "2023-05-16"
+__author__ = "MikePratt"
+
+
+
+# %% --------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+from huggingsound import SpeechRecognitionModel
+
+# %% --------------------------------------------------------------------------
+# Download the model and initiate the audio path
+# -----------------------------------------------------------------------------
+model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english")
+audio_paths = ['audio_files/harvard.wav', 'audio_files/jackhammer.wav']
+
+# %% --------------------------------------------------------------------------
+# Test the model
+# -----------------------------------------------------------------------------
+transcriptions = model.transcribe(audio_paths)
+
+
+
+# %% --------------------------------------------------------------------------
+# 
+# -----------------------------------------------------------------------------
+import whisper
