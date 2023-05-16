f9010adf1af31b743f85ee4101f9d6e089614aae
mikepratt1 <118982716+mikepratt1@users.noreply.github.com>
2023-05-16 16:06:47 +0100

    more details to the gradio app

--- main.py
+++ main.py
@@ -14,21 +14,34 @@ __author__ = "MikePratt"
 # -----------------------------------------------------------------------------
 import gradio as gr
 import whisper # add to requirements.txt
+from huggingsound import SpeechRecognitionModel
 
-def transcription(audio_file):
+def transcribe(audio_file):
     
     try:
-        model = whisper.load_model("base")
-        result = model.transcribe(audio_file)
-        return result["text"]
+        model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english")
+        audio_paths = [audio_file]
+        transcriptions = model.transcribe(audio_paths)
+        return transcriptions[0]['transcription']
 
     except:
         return "There seems to be an error with your audio file, please ensure the following:\n-File is .wav or .mp4\n-You've not been an idiot"
     
-    
-demo = gr.Interface(fn=transcription, 
-                    inputs=gr.Audio(label="Audio File"), 
-                    outputs=gr.Text())
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            audio = gr.Audio(label=".wav Audio File")
+            transcribe_btn = gr.Button(value="Transcribe")
+        with gr.Column():
+            transcription = gr.Textbox(label="Transcription")
+
+    transcribe_btn.click(transcribe, inputs=audio, outputs=transcription)
+
+
+        
+# demo = gr.Interface(fn=transcription, 
+#                     inputs=gr.Audio(label="Audio File"), 
+#                     outputs=gr.Text())
 
 if __name__ == "__main__":
     demo.launch()
